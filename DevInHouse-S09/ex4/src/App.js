687e705ba08bf8d6515c69644823fda7fb7b9935
Renato <renato.bastos96@gmail.com>
2021-12-14 19:36:04 -0300

    Update App.js

--- DevInHouse-S09/ex4/src/App.js
+++ DevInHouse-S09/ex4/src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const valorAceso = "0px 0px 70px yellow, inset 0 0 50px orange";
   const valorApagado = "0px 0px 0px yellow";
+  
   return (
     <div id="container">
       <Lampada luz={aceso} />
