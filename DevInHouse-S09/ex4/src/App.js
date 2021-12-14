f0aedbb0f4f2198fa9d6f301f081adf006f71a58
Renato <renato.bastos96@gmail.com>
2021-12-14 12:38:31 -0300

    Semana09

---
+++ DevInHouse-S09/ex4/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "500px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: aceso ? "#ffee00" : "#000",
+      }}
+    >
+      <button onClick={}>Mudar</button>
+    </div>
+  );
+}
+
+export default App;
