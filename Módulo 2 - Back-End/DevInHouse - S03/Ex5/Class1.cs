479cf7cd23e63043a9bc1b0fb8b66197850aabac
Renato Bastos <renato.bastos96@gmail.com>
2022-02-27 11:26:26 -0300

    S03 - Ex5

---
+++ Módulo 2 - Back-End/DevInHouse - S03/Ex5/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Calculadora
+{
+	private double _valor1;
+	private double _valor2;
+	private string _operador;
+
+	public double Valor1
+    {
+		get { return _valor1; }
+		set { _valor1 = value; }
+    }
+
+	public double Valor2
+    {
+		get { return _valor2; }
+		set { _valor2 = value; }
+    }
+
+	public string Operador
+    {
+		get { return _operador; }
+		set { _operador = value; }
+    }
+
+	public string Calcular()
+    {
+		if (_operador == "+")
+        {
+			return $"Soma: {_valor1 + _valor2}";
+
+        } else if (_operador == "-")
+		{
+			return $"Subtração: {_valor1 - _valor2}";
+
+		} else if (_operador == "*")
+		{
+			return $"Multiplicação: {_valor1 * _valor2}";
+
+		}
+		else if (_operador == "/")
+		{
+			return $"Divisão: {_valor1 / _valor2}";
+
+		} else
+        {
+			return "Operador Inválido";
+        }
+
+	}
+}
---
+++ Módulo 2 - Back-End/DevInHouse - S03/Ex5/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Calculadora calculo1 = new Calculadora();
+calculo1.Valor1 = 2;
+calculo1.Valor2 = 3;
+calculo1.Operador = "+";
+Console.WriteLine(calculo1.Calcular());
+
+Calculadora calculo2 = new Calculadora();
+calculo2.Valor1 = 2;
+calculo2.Valor2 = 3;
+calculo2.Operador = "-";
+Console.WriteLine(calculo2.Calcular());
+
+Calculadora calculo3 = new Calculadora();
+calculo3.Valor1 = 2;
+calculo3.Valor2 = 3;
+calculo3.Operador = "*";
+Console.WriteLine(calculo3.Calcular());
+
+Calculadora calculo4 = new Calculadora();
+calculo4.Valor1 = 3;
+calculo4.Valor2 = 2;
+calculo4.Operador = "/";
+Console.WriteLine(calculo4.Calcular());
