f8286c78aa776879696b9de7a440b7714da9830b
Renato Bastos <renato.bastos96@gmail.com>
2022-02-28 15:51:24 -0300

    S03 - Ex10

---
+++ Módulo 2 - Back-End/DevInHouse - S03/Ex10/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class Animal
+{
+	private string _name;
+    private string _type;
+
+	public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public string Type
+    {
+        get { return _type; }
+
+        set {
+            if(value == "cachorro" || value == "peixe" || value == "gato")
+            {
+                _type = value;
+            } else
+            {
+                Console.WriteLine("Tipo inválido");
+            }
+           
+             }
+    }
+
+    public void VerificarTipo(string tipo)
+    {
+        if(tipo == _type)
+        {
+            Console.WriteLine($"O animais é um {tipo}");
+        } else
+        {
+            Console.WriteLine($"O animais não é um {tipo}");
+        }
+    }
+}
+
+
+public class AnimalList
+{
+    private List<Animal> _lista;
+
+    public void adicionarAnimal(Animal animal)
+    {
+        if (_lista == null)
+        {
+            _lista = new List<Animal>();
+        }
+
+        _lista.Add(animal); 
+    }
+
+    public void contagemCachorros()
+    {
+        int count = 0;
+
+        foreach(Animal animal in _lista)
+        {
+            if(animal.Type == "cachorro")
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Número de cachorros: {count}");
+    }
+
+}
---
+++ Módulo 2 - Back-End/DevInHouse - S03/Ex10/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Animal animal1 = new Animal();
+animal1.Name = "Mimis";
+animal1.Type = "gato";
+
+Animal animal2 = new Animal();
+animal2.Name = "Sol";
+animal2.Type = "cachorro";
+
+Animal animal3 = new Animal();
+animal3.Name = "Sol";
+animal3.Type = "cachorro";
+
+
+AnimalList lista = new AnimalList();
+lista.adicionarAnimal(animal1);
+lista.adicionarAnimal(animal2);
+lista.adicionarAnimal(animal3);
+lista.adicionarAnimal(animal2);
+
+lista.contagemCachorros();
