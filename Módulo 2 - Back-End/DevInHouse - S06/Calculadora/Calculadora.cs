be6ed76b8c9cca3b09515f320c68e9eb1a4383aa
Renato Bastos <renato.bastos96@gmail.com>
2022-03-18 22:09:21 -0300

    S05

---
+++ Módulo 2 - Back-End/DevInHouse - S06/Calculadora/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        public float Memoria1 { get; set; }
+        public float Memoria2 { get; set; }
+        public string Operacao { get; set; }
+
+        public float Somar()
+        {
+            return Memoria1 + Memoria2;
+        }
+
+        public float Subtrair()
+        {
+            return Memoria1 - Memoria2;
+        }
+
+        public float Multiplicar()
+        {
+            return Memoria1 * Memoria2;
+        }
+
+        public float Dividir()
+        {
+            return Memoria1 / Memoria2;
+        }
+
+
+    }
+}
